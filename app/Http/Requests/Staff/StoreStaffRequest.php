ae081586819762be6371a5892e51ddb95f4aae30
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-07-20 16:49:32 +0800

    feat(staff): add staff form request validation

---
+++ app/Http/Requests/Staff/StoreStaffRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Staff;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use App\Concerns\PasswordValidationRules;
+use App\Concerns\ProfileValidationRules;
+use App\Enums\RoleEnum;
+use Illuminate\Validation\Rule;
+
+class StoreStaffRequest extends FormRequest
+{
+    use PasswordValidationRules, ProfileValidationRules;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            ...$this->profileRules(isPrecognitive: $this->isPrecognitive()),
+            'password' => $this->passwordRules(),
+            'role' => ['required', 'string', Rule::in(RoleEnum::staffValues())],
+        ];
+    }
+}
