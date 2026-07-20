f15a410a8090fb2bc5bd34c5c08795d55a631e3b
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-07-20 16:50:08 +0800

    feat(staff): register staff routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Enums\PermissionEnum;
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\CustomerController;
+use App\Http\Controllers\Admin\StaffController;
 use App\Http\Controllers\Auth\RegistrationValidationController;
 use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Support\Facades\Route;
@@ -12,21 +13,44 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 
     Route::prefix('admin')->name('admin.')->group(function (): void {
-        Route::resource('users', UserController::class)
-            ->only(['index', 'show'])
-            ->middleware('permission:' . PermissionEnum::STAFF_VIEW->value);
-
-        Route::resource('users', UserController::class)
+        // --- Staff ---
+        Route::resource('staff', StaffController::class)
             ->only(['create', 'store'])
+            ->parameters(['staff' => 'staff'])
             ->middleware('permission:' . PermissionEnum::STAFF_CREATE->value);
 
-        Route::resource('users', UserController::class)
+        Route::resource('staff', StaffController::class)
             ->only(['edit', 'update'])
+            ->parameters(['staff' => 'staff'])
             ->middleware('permission:' . PermissionEnum::STAFF_UPDATE->value);
 
-        Route::resource('users', UserController::class)
+        Route::resource('staff', StaffController::class)
+            ->only(['index', 'show'])
+            ->parameters(['staff' => 'staff'])
+            ->middleware('permission:' . PermissionEnum::STAFF_VIEW->value);
+
+        Route::resource('staff', StaffController::class)
             ->only(['destroy'])
+            ->parameters(['staff' => 'staff'])
             ->middleware('permission:' . PermissionEnum::STAFF_DEACTIVATE->value);
+
+
+        // --- Customers ---
+        Route::resource('customers', CustomerController::class)
+            ->only(['index', 'show'])
+            ->parameters(['customers' => 'customer'])
+            ->middleware('permission:' . PermissionEnum::CUSTOMERS_VIEW->value);
+
+        Route::resource('customers', CustomerController::class)
+            ->only(['edit', 'update'])
+            ->parameters(['customers' => 'customer'])
+            ->middleware('permission:' . PermissionEnum::CUSTOMERS_UPDATE->value);
+
+
+        Route::resource('customers', CustomerController::class)
+            ->only(['destroy'])
+            ->parameters(['customers' => 'customer'])
+            ->middleware('permission:' . PermissionEnum::CUSTOMERS_BLACKLIST->value);
     });
 });
 
